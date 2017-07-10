681f06146b53285357e4fe673b5075ae0b72962f
DanilF <danil_flores@ultimatesoftware.com>
2017-07-09 21:56:42 -0400

    Exporting options.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { graphqlClient } from './mongo-grqphql-client-factory';
 
-export { MongoGraphQLClient } from './mongo-graphql-client';
+export { MongoGraphQLClient, GraphQLMongoClientOptions } from './mongo-graphql-client';
 export { graphqlToMongo } from './graphql-to-mongo';
 export { findOne, findAll, findMultiple } from './mongo-queries';
 
